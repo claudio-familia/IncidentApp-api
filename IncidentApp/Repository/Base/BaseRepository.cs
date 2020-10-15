5bce93018f52deda2b7e4a2497e18aed1bda90c5
Claudio Junior Familia Morel <cfamilia@mardom.com>
2020-10-14 23:57:24 -0400

    Create and implement base repository

---
+++ IncidentApp/Repository/Base/BaseRepository.cs
@@ -0,0 +1,61 @@
+﻿using IncidentApp.Models.Context;
+using IncidentApp.Repository.Base.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace IncidentApp.Repository.Base
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        private readonly IncidentContext context;
+        private readonly DbSet<T> db;
+
+        public BaseRepository(IncidentContext _context)
+        {
+            context = _context;
+            db = _context.Set<T>();
+        }
+
+        public T Create(T entity)
+        {
+            db.Add(entity);
+            
+            context.SaveChanges();
+
+            return entity;
+        }
+
+        public bool Exists(Func<IQueryable<T>, IQueryable<T>> transform, Expression<Func<T, bool>> filter = null)
+        {
+            return db.Where(filter).Count() > 0;
+        }
+
+        public IEnumerable<T> Find(Func<IQueryable<T>, IQueryable<T>> transform, Expression<Func<T, bool>> filter = null)
+        {
+            return db.Where(filter).AsEnumerable();
+        }
+
+        public T Read(int id)
+        {
+            return db.Find(id);
+        }
+
+        public IEnumerable<T> Read()
+        {
+            return db.AsEnumerable();
+        }
+
+        public T Update(T entity)
+        {
+            db.Update(entity);
+
+            context.SaveChanges();
+
+            return entity;
+        }
+    }
+}
---
+++ IncidentApp/Repository/Base/Contracts/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace IncidentApp.Repository.Base.Contracts
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        T Create(T entity);
+        T Update(T entity);
+        T Read(int id);
+        IEnumerable<T> Read();
+        IEnumerable<T> Find(Func<IQueryable<T>, IQueryable<T>> transform, Expression<Func<T, bool>> filter = null);
+        bool Exists(Func<IQueryable<T>, IQueryable<T>> transform, Expression<Func<T, bool>> filter = null);
+    }
+}
