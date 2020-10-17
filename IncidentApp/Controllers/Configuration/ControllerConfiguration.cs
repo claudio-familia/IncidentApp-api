cb8ac9b184e8053bc15854ade246833c29af536c
Claudio Junior Familia Morel <cfamilia@mardom.com>
2020-10-17 18:16:12 -0400

    Add Configuration clases to dependency injection

---
+++ IncidentApp/Controllers/Configuration/ControllerConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IncidentApp.Controllers.Configuration
+{
+    public class ControllerConfiguration
+    {
+    }
+}
