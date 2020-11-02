959d6645f431e971a3f93e1aa6c0722ad222baf5
Claudio Junior Familia Morel <cfamilia@mardom.com>
2020-11-02 08:03:55 -0400

    Add status enum

--- IncidentApp/Models/Context/IncidentContext.cs
+++ IncidentApp/Models/Context/IncidentContext.cs
@@ -1,4 +1,5 @@
 ﻿using IncidentApp.Models.Configurations;
+using IncidentApp.Models.Enums;
 using IncidentApp.Services.Contracts;
 using IncidentApp.Utils;
 using Microsoft.EntityFrameworkCore;
@@ -37,7 +38,8 @@ namespace IncidentApp.Models.Context
                 {
                     Id = 1,
                     Username = "Administrator",
-                    Password = cryptography.Encrypt("1234", configuration["Authentication:SecretKey"])
+                    Password = cryptography.Encrypt("1234", configuration["Authentication:SecretKey"]),
+                    Status = StatusEnum.Active.ToString()
                 });
             });
         }
---
+++ IncidentApp/Models/Enums/StatusEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IncidentApp.Models.Enums
+{
+    public enum StatusEnum
+    {
+        Inactive,
+        Active
+    }
+}
